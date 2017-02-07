82b9ba7e1dd91f929c9959158e48b3e19a00f1bf
Dalimil Hajek <dalimilhajek@gmail.com>
2017-02-07 17:01:44 +0000

    Add image assets

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+
+
+const images = {
+  "avatar": require("./avatar.jpg"),
+  "defaultAvatar": require("./default-avatar.png"),
+  "placeholder": require("./image-placeholder.jpg"),
+  "placeholderSquare": require("./image-placeholder-square.jpg"),
+  "sidebarA": require("./sidebar-1.jpg"),
+  "sidebarB": require("./sidebar-2.jpg"),
+  "sidebarC": require("./sidebar-3.jpg"),
+  "sidebarD": require("./sidebar-4.jpg"),
+  "cardA": require("./card-1.jpg"),
+  "cardB": require("./card-2.jpg"),
+  "cardC": require("./card-3.jpg")
+};
+
+export default images;
