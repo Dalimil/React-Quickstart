ed095ea5c8b23f0b40248326dc3ef1da294d7a18
Dalimil Hajek <dalimilhajek@gmail.com>
2017-02-20 21:28:50 +0000

    Add Firebase Storage demo

--- src/firebase.js
+++ src/firebase.js
@@ -59,3 +59,61 @@ export { Auth };
 
 
 
+class FileStorageDemo {
+  constructor() {
+    this.storage = firebase.storage();
+  }
+
+  /**
+   * Assume this HTML:
+   * <input type="file" value="upload" id="fileButton" />
+   */
+  fileUploadDemo() {
+    $("#fileButton").on('change', e => {
+      const file = e.target.files[0];
+      const uploadTask = this.storage.ref().child(`images/${file.name}`).put(file);
+      
+      uploadTask.on('state_changed',
+        function progress(snapshot) {
+          const percent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + percent + '% done');
+        },
+        function error(err) {
+          // Handle unsuccessful uploads
+        },
+        function complete() {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          const downloadURL = uploadTask.snapshot.downloadURL;
+        }
+      );
+    });
+  }
+
+  /**
+   * File download - need to configure CORS first (for XHR):
+   * https://firebase.google.com/docs/storage/web/download-files#cors_configuration
+   */
+  fileDownloadDemo() {
+    this.storage.ref().child('images/stars.jpg').getDownloadURL().then(function(url) {
+      // `url` is the download URL for 'images/stars.jpg'
+
+      // This can be downloaded directly:
+      const xhr = new XMLHttpRequest();
+      xhr.responseType = 'blob';
+      xhr.onload = function(event) {
+        const blob = xhr.response;
+      };
+      xhr.open('GET', url);
+      xhr.send();
+
+      // Or inserted into an <img> element:
+      const img = document.getElementById('myimg');
+      img.src = url;
+    }).catch(function(error) {
+      // Handle any errors
+    });
+  }
+}
+
+
