f10a0a473ea93dcc6012d3ec0b29bbe985d9fd46
Dalimil Hajek <dalimilhajek@gmail.com>
2017-05-20 20:01:11 +0100

    Add react-transition-group package

---
+++ src/demos/animation.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ['hello', 'world', 'click', 'me']
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    const newItems = this.state.items.concat([
+      prompt('Enter some text')
+    ]);
+    this.setState({ items: newItems });
+  }
+
+  handleRemove(i) {
+    let newItems = this.state.items.slice();
+    newItems.splice(i, 1);
+    this.setState({ items: newItems });
+  }
+
+  render() {
+    const items = this.state.items.map((item, i) => (
+      <div key={item} onClick={() => this.handleRemove(i)}>
+        {item}
+      </div>
+    ));
+
+    return (
+      <div>
+        <button onClick={this.handleAdd}>Add Item</button>
+        <CSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}
+          >{/* transitionAppear={true} transitionAppearTimeout={500} */}
+          {items}
+        </CSSTransitionGroup>
+      </div>
+    );
+  }
+}
+
+/*
+.example-enter {
+  opacity: 0.01;
+}
+
+.example-enter.example-enter-active {
+  opacity: 1;
+  transition: opacity 500ms ease-in;
+}
+
+.example-leave {
+  opacity: 1;
+}
+
+.example-leave.example-leave-active {
+  opacity: 0.01;
+  transition: opacity 300ms ease-in;
+}
+*/
